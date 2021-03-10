cffd500791eff5062c023d67401f1d0869617e65
Leobardo Ernesto Sanchez Cruz <Leobardo.Sanchez@uabc.edu.mx>
2021-03-09 22:38:00 -0800

    Hangman V1

---
+++ README.md
@@ -0,0 +1,7 @@
+# Bienvenido al juegos del Ahorcado
+    Contenido del programa:
+- Hangman.py
+- Words.py
+- README.md
+
+Programa iniciado en clase de la materia de programacion en python 
---
+++ hangman.py
@@ -0,0 +1,28 @@
+# Import modules to use
+from words import palabras
+import random
+
+# 1. Welcome
+print("Welcome to the game hangman in Python")
+
+# 2. Function to get word
+def get_valid_word(palabras):
+  word = random.choice(palabras)
+
+  # Choose a good word
+  while '-' in word or ' ' in word: # While - or ' '
+    word = random.choice(palabras)
+
+  return word
+
+# Display word and its length with lines
+my_word = get_valid_word(palabras)
+print(my_word + '\n',len(my_word))
+
+
+
+# Una función que despliegue los guiones
+# dependiendo el tamaño de la palabra
+# Ejemplo:
+# A N O N Y M O U S
+# _ _ _ _ _ _ _ _ _
