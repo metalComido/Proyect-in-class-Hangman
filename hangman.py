77d75bcf003a8ba070b55683a5e7977709324ed7
Leobardo Ernesto Sanchez Cruz <Leobardo.Sanchez@uabc.edu.mx>
2021-03-09 22:40:09 -0800

    Hangman V2 add function despliege

--- hangman.py
+++ hangman.py
@@ -19,10 +19,15 @@ def get_valid_word(palabras):
 my_word = get_valid_word(palabras)
 print(my_word + '\n',len(my_word))
 
-
-
 # Una función que despliegue los guiones
 # dependiendo el tamaño de la palabra
 # Ejemplo:
 # A N O N Y M O U S
 # _ _ _ _ _ _ _ _ _
+def despliege():
+  for char in my_word:
+    print(char, end=" ")
+  print('')
+  for char in my_word:
+    print("_", end=" ")
+despliege()
