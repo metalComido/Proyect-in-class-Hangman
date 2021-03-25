b3d14b62f0fd96e7e9a26955fc809bb7744c53f9
Leobardo Ernesto sanchez Cruz <Leobardo.sanchez@uabc.edu.mx>
2021-03-25 14:42:35 -0700

    hangman virtual environment pipenv intalling and using colorama and Django

--- README.md
+++ README.md
@@ -1,7 +1,27 @@
-# Bienvenido al juegos del Ahorcado
-    Contenido del programa:
-- Hangman.py
-- Words.py
-- README.md
+# Welcome to the Hangman Games
 
-Programa iniciado en clase de la materia de programacion en python 
+Note : Program incomplete only for  Practice in class programing in python 
+
+    Program Content:
+- hangman.py
+- words.py
+- README.md
+- hello
+- requirements.txt
+- pipfile
+- pipfile.lock
+
+
+Programa iniciado en clase de la materia de programacion en python.
+
+update 1.1
+- A feature that deploys "_" depending on the size of the word
+Example: 
+
+.ANONYMOUS.
+
+. _ _ _ _ _ _ _ _ _ .
+ 
+
+Update 1.2
+- Deploying pipenv virtual environment with libraries[dev-packages] colorama and Django [packages] and using colorama for coloring a linea of string when print _ _ _ _ _ _ 
--- hangman.py
+++ hangman.py
@@ -1,33 +1,33 @@
-# Import modules to use
-from words import palabras
+from colorama import Fore,Back,Style,init
+from words import Words
 import random
+init()
 
 # 1. Welcome
-print("Welcome to the game hangman in Python")
+print("Welcome to the game of hangman in Python")
 
-# 2. Function to get word
-def get_valid_word(palabras):
-  word = random.choice(palabras)
+# 2. Function to get Words from words.py
+def get_valid_word(Words):
+  word = random.choice(Words)
 
-  # Choose a good word
+# Choose a good word
   while '-' in word or ' ' in word: # While - or ' '
-    word = random.choice(palabras)
-
+    word = random.choice(Words)
   return word
 
 # Display word and its length with lines
-my_word = get_valid_word(palabras)
+my_word = get_valid_word(Words)
 print(my_word + '\n',len(my_word))
 
-# Una función que despliegue los guiones
-# dependiendo el tamaño de la palabra
-# Ejemplo:
+# A feature that deploys _
+# depending on the size of the word
+# Example:
 # A N O N Y M O U S
 # _ _ _ _ _ _ _ _ _
-def despliege():
-  for char in my_word:
-    print(char, end=" ")
-  print('')
-  for char in my_word:
-    print("_", end=" ")
-despliege()
+
+for char in my_word:
+  print(char, end=" ")
+print("")
+for char in my_word:
+  print(Fore.CYAN + "_", end=" ")
+print("")
